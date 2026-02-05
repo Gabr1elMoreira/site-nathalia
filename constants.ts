900ba439e37279efb7028a29a96442b74ed32ad6
Gabriel Moreira <gabrielmoreira486@gmail.com>
2026-02-05 17:27:20 -0300

    ajuste Nati

--- constants.ts
+++ constants.ts
@@ -100,13 +100,13 @@ export const SERVICES: Service[] = [
 
   {
     id: '9',
-    title: 'Design de Sobrancelha',
-    description: 'Aplicação de um fio sintético sobre cada fio natural. Resultado ultra natural e elegante.',
-    fullDescription: 'A elegância da naturalidade. Nesta técnica, acoplamos um fio de extensão para cada fio natural seu. O objetivo é alongar, curvar e dar um leve volume, imitando seus cílios naturais, porém perfeitos. Ideal para o dia a dia e para quem nunca fez extensão.',
+    title: 'Design de Sobrancelha Personalizado',
+    description: 'Técnica que valoriza o formato natural do rosto, respeitando simetria e proporções.',
+    fullDescription: 'Realça a expressão e harmoniza o olhar de forma sutil e personalizada.',
     duration: '1h 30min',
     maintenance: '15 a 20 dias',
-    image: 'Classico.jpg',
-    priceEstimate: 'R$ 150,00'
+    image: 'DesignSobrancelhas.jpeg',
+    priceEstimate: 'R$ 50,00'
   }
 ];
 
